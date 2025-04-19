0fa81a9849577eeaaaf4382d091bd8f86f04a886
KyleW888 <kwucontact@gmail.com>
2025-04-19 19:37:24 -0400

    Create matrix_c.h

---
+++ matrix_project/matrix_c.h
@@ -0,0 +1,20 @@
+
+#ifndef MATRIX_C_H
+#define MATRIX_C_H
+
+typedef struct { //basically struct Matrix
+    int rows;
+    int columns;
+    double** data;
+} Matrix;
+
+Matrix create_matrix(int rows, int cols);  // C constructor
+void free_matrix(Matrix* m);               // C destructor
+void display(Matrix* m);
+void add(Matrix* result, Matrix* A, Matrix* B);
+void scalarMultiply(Matrix* result, Matrix* A, double scalar);
+void multiply(Matrix* result, Matrix* A, Matrix* B);
+void transpose(Matrix* result, Matrix* A);
+
+
+#endif
