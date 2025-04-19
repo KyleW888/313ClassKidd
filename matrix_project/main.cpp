48a9640c28dc9c86e0ef98df7ebd04e1ae5f415a
KyleW888 <kwucontact@gmail.com>
2025-04-19 18:53:16 -0400

    Update main.cpp

--- matrix_project/main.cpp
+++ matrix_project/main.cpp
@@ -1,6 +1,6 @@
-//Hard coded matrices \
 #include "matrix.h"
 #include <iostream>
+#include <stdexcept>  // Include for exception handling
 using namespace std;
 
 int main() {
@@ -9,7 +9,7 @@ int main() {
     A.data[0][1] = 4;
     A.data[1][0] = 8;
     A.data[1][1] = 3;
-
+    
     Matrix B(2, 3);
     B.data[0][0] = 1;
     B.data[0][1] = 2;
@@ -18,7 +18,6 @@ int main() {
     B.data[1][1] = 5;
     B.data[1][2] = 6;
 
-  
     Matrix C(2, 3);
     C.data[0][0] = 2;
     C.data[0][1] = 4;
@@ -27,23 +26,29 @@ int main() {
     C.data[1][1] = 3;
     C.data[1][2] = 5;
 
-  
-    Matrix threeB = B.scalarMultiply(3);
-    cout << "3 * Matrix B:" << endl;
-    threeB.display();
+    try {
+    
+        Matrix threeB = B.scalarMultiply(3);
+        cout << "3 * Matrix B:" << endl;
+        threeB.display();
 
-    Matrix CT = C.transpose();
-    cout << "Matrix C Transposed (C^T):" << endl;
-    CT.display();
+        Matrix CT = C.transpose();
+        cout << "Matrix C Transposed (C^T):" << endl;
+        CT.display();
 
-   
-    Matrix product = threeB.multiply(CT);
-    cout << "(3 * Matrix B) * Matrix C^T:" << endl;
-    product.display();
+     
+        Matrix product = threeB.multiply(CT);
+        cout << "(3 * Matrix B) * Matrix C^T:" << endl;
+        product.display();
 
-    Matrix D = A.add(product);
-    cout << "Matrix D = A + (3 * B) * C^T:" << endl;
-    D.display();
+      
+        Matrix D = A.add(product);
+        cout << "Matrix D = A + (3 * B) * C^T:" << endl;
+        D.display();
+    }
+    catch (const std::invalid_argument& e) {
+        cout << "Error: " << e.what() << endl;
+    }
 
     return 0;
 }
